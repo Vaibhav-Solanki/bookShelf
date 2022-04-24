bd587af2e74409127477df30825de611a6c788a5
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-04-24 10:44:45 +0530

    final

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,36 +1,18 @@
 import React from "react";
 import "./Carousel.css";
 import Logo from "../../images/logo.png";
-import book from "../../images/book.jpg";
+// import book from "../../images/book.jpg";
 export default function Caro() {
   return (
     <div className="px-4 py-5 my-2 text-center">
-      <img
-        className="d-block mx-auto mb-4"
-        src={Logo}
-        alt=""
-        width={72}
-        height={72}
-      />
-      <h1 className="display-5 fw-bold">Project Xyz</h1>
+      <img className="d-block mx-auto mb-4" src={Logo} width={72} height={72} />
+      <h1 className="display-5 fw-bold">The BookMark</h1>
       <div className="col-lg-6 mx-auto">
         <p className="lead mb-4">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste, veniam
-          provident aspernatur hic numquam fugit consequuntur dolorum explicabo
-          sint earum ipsa vero blanditiis rerum magnam, voluptatem eos unde
-          culpa ipsam.
+          Welcome to BookMark ! Here you can read your favorite books and
+          interact with others who loves reading books and have similar taste.
+          Immerse yourself into the world of books and enjoy!
         </p>
-        {/* <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
-            Primary button
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-secondary btn-lg px-4"
-          >
-            Secondary
-          </button>
-        </div> */}
       </div>
     </div>
   );
