6feaa168cd891d277554f9f8c03a58e234663522
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-04-26 20:09:04 +0530

    Update slider.jsx

--- src/Components/SLider/slider.jsx
+++ src/Components/SLider/slider.jsx
@@ -5,10 +5,14 @@ import "./slider.css";
 const Slider = () => {
   const [current, setCurrent] = useState(0);
   const length = SliderData.length;
-  const nextSlide = () => {
-    setCurrent((pre) => (pre == length - 1 ? 0 : pre + 1));
-  };
-  setInterval(nextSlide, 4000);
+  setInterval(
+    () =>
+      setTimeout(() => {
+        setCurrent((pre) => (pre == length - 1 ? 0 : pre + 1));
+      }, 1000),
+    4000
+  );
+
   return (
     <div className="container d-flex justify-content-center">
       <div className="slide w-100">
